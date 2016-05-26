70508d0fce11656d75f084c0673459ea0e02e83d
xbc <463449055@qq.com>
2016-05-26 14:37:17 +0800

    update

---
+++ build/js/common.js
@@ -0,0 +1,32 @@
+;(function(w,d){
+	function jQuery(selector) {
+		var _domObject = {};
+		if(/^#/.test( selector )) {
+			_domObject.dom = document.getElementById(selector.replace(/^#/,''));
+		}else if(/^\./.test( selector )) {
+			_domObject.doms = p.getElementByClass(selector.replace(/^\./,''));
+		}
+		return _domObject;
+	}
+	var p = jQuery.prototype;
+	p.each = function( o, cb ) {
+		//console.log(typeof o);
+		if(o.constructor === Array){ 
+			var length = o.length, i = 0;
+			for( ; i < length; i++){
+				cb.call( o[i], i, o[i] );
+			}
+		}
+	}
+	p.getElementByClass = function(className, parent) {
+		  parent || (parent = document);
+		  var descendants= parent.getElementsByTagName('*'), i=-1, e, result=[];
+		  while (e=descendants[++i]) {
+		    ((' '+(e['class']||e.className)+' ').indexOf(' '+className+' ') > -1) && result.push(e);
+		  }
+		  return result;
+	}
+	window['$'] = jQuery;
+})(window,document);
+
+
